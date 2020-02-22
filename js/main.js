59dec093c9aab136d09bb6345b36b8227eb1d009
nod510nod <nod510nod@Najees-MBP.fios-router.home>
2020-02-22 18:57:20 -0500

    v2 -adds delete

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 function userInput(e){
 //prevent the submit button from going the next page.
 e.preventDefault();
-
 //get the vlaue from the front end
 var inputBox = document.querySelector('input[type="text"]').value;
-
 //create new element
   var newli = document.createElement('li');
   //add class
@@ -13,51 +11,36 @@ var inputBox = document.querySelector('input[type="text"]').value;
   var liNode = document.createTextNode(inputBox);
   //add node to element
   newli.appendChild(liNode);
-
   //create button
   var redButton = document.createElement('button');
   redButton.className = "btn btn-danger btn-sm float-right";
   redButton.setAttribute('type','button');
   redButton.setAttribute('name','button');
   var redButtonText = document.createTextNode('X');
-
   //add text to button
   redButton.appendChild(redButtonText);
-
   // //get UL
    var getUL = document.getElementById('items');
-
   // //add to last element
   getUL.appendChild(newli);
-
   //add button to last list item
   getUL.appendChild(newli).appendChild(redButton);
-
   //clear text input
   if(inputBox.length > 0){
     document.querySelector('input[type="text"]').value = "";
   }
-
+  //function to hide line item
+function deleteLine() {
+  this.parentElement.style.display = "none";
 };
-
+  //grab the close button from front end
+  var closeBtn = document.getElementsByClassName("btn btn-danger btn-sm float-right");
+  //loop through each ul element
+  for(var i = 0; i < getUL.children.length; i++){
+    closeBtn[i].addEventListener('click',deleteLine,false);
+  };
+};
   //get the submit button from the frontend
   var submitButton = document.querySelector('button[type="submit"]');
   //onclick the userInput function fires
   submitButton.addEventListener('click',userInput,false);
-
-  //function to hide line item
-  function deleteLine() {
- this.parentElement.style.display = "none";
-};
-
-//grab the close button from front end
-var closeBtn = document.getElementsByClassName("btn btn-danger btn-sm float-right");
-
-//get length of the items in the UL
- var ulLength = document.getElementsByTagName('li');
-
-//loop through each ul element 
-for(var i = 0; i < ulLength.length; i++){
-closeBtn[i].addEventListener('click',deleteLine,false);
-
-};
